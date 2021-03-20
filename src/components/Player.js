a42a43a21a11aedd2d9e06aa5933a957ab255cd7
bhandeystruck <adixb@hotmail.co.uk>
2021-03-19 21:56:15 -0400

    first commit

---
+++ src/components/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Player=()=> {
+    return (
+        <div className="player">
+            
+        </div>
+    )
+}
+
+export default Player
---
+++ src/components/Songs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Player from './Player'
+
+const Songs=()=> {
+    return (
+        <div className="song">
+        <Player/>
+            
+        </div>
+    )
+}
+
+export default Songs
